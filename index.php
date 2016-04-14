8983d8cd3fa3d0818b43a185350006143addfa29
jeyfost <foster_andrew@tut.by>
2016-04-14 15:25:40 +0300

    Первый коммит шаблона

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="создание сайтов, дизайн, web дизайн, логотип, фирменный стиль, реклама, дизайн сайтов, айдентика, разработка сайта, pr, дизайн упаковки, лого, бренд, брендинг, брендбук, название, нейминг, копирайтинг">
+    <meta name="description" content="Студия дизайна AIRLAB. Создание и сопровождение сайтов. Разработка фирменного стиля, брендирование, рекламные компании.">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+    <title>Студия дизайна AIRLAB</title>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
