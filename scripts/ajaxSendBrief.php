30364b2469dc84beeb2f4f576dad5158c5a1d71c
jeyfost <foster_andrew@tut.by>
2016-06-22 15:09:36 +0300

    Добавлено уведомление по email о поступлении нового брифа

--- scripts/ajaxSendBrief.php
+++ scripts/ajaxSendBrief.php
@@ -19,7 +19,28 @@ $contacts = trim(htmlspecialchars($_POST['contacts']));
 $person = trim(htmlspecialchars($_POST['person']));
 
 if($mysqli->query("INSERT INTO brief (name, about, advantage, competitors, competitorsAdvantage, client, advertise, service, idea, help, terms, budget, additions, contacts, person, date) VALUES ('".$name."', '".$about."', '".$advantage."', '".$competitors."', '".$competitorsAdvantage."', '".$client."', '".$advertise."', '".$service."', '".$idea."', '".$help."', '".$terms."', '".$budget."', '".$additions."', '".$contacts."', '".$person."', '".date('Y-m-d H:i:s')."')")) {
-    echo "a";
+    $from = "Сайт airlab";
+    $subject = "Новый бриф";
+    $text = "Поступил новый бриф.";
+    $to = "foster_andrew@tut.by";
+    $email = "no-reply@airlab.by";
+
+    $hash = md5(date('r', time()));
+
+    $headers = "From: ".$from."\nReply-To: ".$email."\nMIME-Version: 1.0";
+    $headers .= "\nContent-Type: multipart/mixed; boundary = \"PHP-mixed-".$hash."\"\n\n";
+
+    $message = "--PHP-mixed-".$hash."\n";
+    $message .= "Content-Type: text/html; charset=\"windows-1251\"\n";
+    $message .= "Content-Transfer-Encoding: 8bit\n\n";
+    $message .= $text."\n";
+    $message .= "--PHP-mixed-".$hash."\n";
+
+    if(@mail($to, $subject, $message, $headers)) {
+        echo "a";
+    } else {
+        echo "b";
+    }
 } else {
     echo "b";
 }
